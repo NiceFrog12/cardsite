57e3c9577a752d717e34bbc5fe886378ad63e77d
NiceFrog12 <156522928+NiceFrog12@users.noreply.github.com>
2024-04-21 12:09:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Импорт
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template,request, redirect, url_for
 #Подключение библиотеки баз данных
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,53 +9,95 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #Создание db
-db = SQLAlchemy(app )
-
-#Задание №1. Создай таблицу БД
+db = SQLAlchemy(app)
+#Создание таблицы
 
 class Card(db.Model):
-    #nullable means if its HAS to be filled, false meaning that user HAS TO fill it in
+    #Создание полей
+    #id
     id = db.Column(db.Integer, primary_key=True)
+    #Заголовок
     title = db.Column(db.String(100), nullable=False)
-    subtitle = db.Column(db.String(100), nullable=False)
+    #Описание
+    subtitle = db.Column(db.String(300), nullable=False)
+    #Текст
     text = db.Column(db.Text, nullable=False)
 
+    #Вывод объекта и id
     def __repr__(self):
-            return f'<Card {self.id}>'
+        return f'<Card {self.id}>'
+    
+
+#Задание №1. Создать таблицу User
+
+class User(db.Model):
+    #userid
+    id = db.Column(db.Integer, primary_key=True)
+    #user email
+    email = db.Column(db.String(50), nullable=False)
+    #user password
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Card {self.id}>'
+
 
 
 
 
 #Запуск страницы с контентом
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def login():
+    error = ''
+    if request.method == 'POST':
+        form_email = request.form['email']
+        form_password = request.form['password']
+            
+        #Задание №4. Реализовать проверку пользователей
+        users_db = User.query.all()
+        for user in users_db:
+            if form_email == user.email and form_password == user.password:
+                return redirect('/index')
+        
+        error = 'Неправильно указан пользователь или пароль'
+        return render_template('login.html', error=error)
+
+            
+    else:
+        return render_template('login.html')
+
+
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        email= request.form['email']
+        password = request.form['password']
+        
+        #Задание №3. Реализовать запись пользователей
+        user = User(email=email, password=password)
+
+        db.session.add(user)
+        db.session.commit()        
+        return redirect('/')
+    
+    else:    
+        return render_template('registration.html')
+
+
+#Запуск страницы с контентом
+@app.route('/index')
 def index():
     #Отображение объектов из БД
-    #Задание №2. Отоброзить объекты из БД в index.html
-    cards = Card.query.all()
-
-    return render_template('index.html',
-                           cards = cards
-                           )
+    cards = Card.query.order_by(Card.id).all()
+    return render_template('index.html', cards=cards)
 
 #Запуск страницы c картой
 @app.route('/card/<int:id>')
 def card(id):
-    #Задание №2. Отоброзить нужную карточку по id
     card = Card.query.get(id)
 
     return render_template('card.html', card=card)
-
-def form_delete(id):
-    #im trying to delete this card, man T-T
-    if request.method == 'SUBMIT':
-        cards = Card.query.all()
-        card = Card.query.get(id)
-        db.session.delete(card)
-        db.session.commit()
-        return render_template('index.html', cards = cards)
-    else:
-        #this is more of a fail state
-        return redirect ('/')
 
 #Запуск страницы c созданием карты
 @app.route('/create')
@@ -70,20 +112,49 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        #Задание №2. Создайте сопосб записи данных в БД
+        #Создание объкта для передачи в дб
 
         card = Card(title=title, subtitle=subtitle, text=text)
+
         db.session.add(card)
-        db.session.commit()       
-
-
-
-        return redirect('/')
+        db.session.commit()
+        return redirect('/index')
     else:
         return render_template('create_card.html')
 
 
+@app.route('/delete/<id>', methods= ['POST'])
+def form_delete(id):
+    #im trying to delete this card, man T-T
+    try:
+        delete = Card.query.get(id)
+        db.session.delete(delete)
+        db.session.commit()
+        return redirect('/index')
+    except:
+        pass
+    
+    return redirect('/index')
+    
+@app.route("/edit/<int:id>", methods=['POST'])
+def cardedit(id):
+    card = Card.query.get(id)
+    return render_template('card_edit.html', card=card)
 
+@app.route("/editthismf", methods = ['POST'])
+def card_edit():
+    # getting info out of the form to edit the card
+    id = request.form['id']
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    text = request.form['text']
+    # update the card in the database
+    card = Card.query.get(id)
+    card.title = title
+    card.subtitle = subtitle
+    card.text = text
+    db.session.commit()
+    return redirect('/index')
 
 
 if __name__ == "__main__":
